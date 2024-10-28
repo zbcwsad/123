3cdbc5edfd7de7008f00a1a67af43b9abe31f30a
zbcwsad <3698463608@qq.com>
2024-10-28 20:54:17 +0800

    对pyprocar画普通能带做了进一步介绍

--- vasp/pyprocar/plot_band.py
+++ vasp/pyprocar/plot_band.py
@@ -1,6 +1,8 @@
 import pyprocar
 ####获取K点信息####
-
+####普通的能带图####
+###fermi能级需要手动修改，data_dir是能带计算目录，需计算完######
+###elimit是能量范围，即Y轴范围###
 data_dir='./band-pyprocar'
 parser = pyprocar.io.Parser(code = 'vasp', dir=data_dir)
 kpath = parser.kpath
