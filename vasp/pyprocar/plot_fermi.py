ea7a852e7c1d75794e834a43e153f24c25da3290
zbcwsad <3698463608@qq.com>
2024-10-31 10:37:50 +0800

    2d fermi surface

--- vasp/pyprocar/plot_fermi.py
+++ vasp/pyprocar/plot_fermi.py
@@ -1,4 +1,7 @@
 import pyprocar
+###########1.scf#############
+###########2.conda activate pyprocar#########
+###########3.run#############
 pyprocar.fermi2D(code = 'vasp', 
                mode='plain',
                fermi=-2.8067,
