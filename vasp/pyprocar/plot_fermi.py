3c173ddff728f0e88107aa9917d058bad2fc585d
zbcwsad <3698463608@qq.com>
2024-10-31 10:35:51 +0800

    2D wfermi surface

---
+++ vasp/pyprocar/plot_fermi.py
@@ -0,0 +1,7 @@
+import pyprocar
+pyprocar.fermi2D(code = 'vasp', 
+               mode='plain',
+               fermi=-2.8067,
+               energy=-0.5,
+               savefig='fermi.png',
+               dirname='./')
